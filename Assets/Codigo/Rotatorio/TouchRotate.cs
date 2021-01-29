289695906b95dccae2371c1a7c4160acd1bda192
Daniel Navarrete <daniel_navarrete_05@hotmail.com>
2021-01-28 21:09:04 -0600

    Puzzle rotatorio (Funcional) Sin cambio se escena

---
+++ Assets/Codigo/Rotatorio/TouchRotate.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchRotate : MonoBehaviour
+{
+    float[] rotations = { 90, 180, 270 };
+
+    void Start()
+    {
+        int rand = Random.Range(0, rotations.Length);
+        transform.eulerAngles = new Vector3(0, 0, rotations[rand]);
+    }
+
+    private void OnMouseDown()
+    {
+        if(!GameControl.youWin)
+        {
+            transform.Rotate(0f, 0f, 90f);
+        }
+    }
+}
