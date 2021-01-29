31b30c2184780c262934b2f85343da442a963099
Fernando747 <fernandojcr5@gmail.com>
2021-01-28 18:01:23 -0600

    Puzzle 15 100% jugable (con random)

--- Assets/Codigo/Puzzle15.cs
+++ Assets/Codigo/Puzzle15.cs
@@ -10,6 +10,8 @@ public class Puzzle15 : MonoBehaviour
     void Start()
     {
         Inicio();
+        for (int i=0; i<999;i++)
+        Shuffle();
     }
 
     void Inicio()
@@ -19,17 +21,23 @@ public class Puzzle15 : MonoBehaviour
         for (int x = 0; x < 4; x++)
             {
                 CajaNumero caja = Instantiate(cajaPrefab, new Vector2(x, y), Quaternion.identity);
-                caja.Inicio(x, y, n + 1, sprites[n], hacerCambio);
+                caja.Inicio(x, y, n + 1, sprites[n], CambiarClick);
+                caja.name = "caja " +n;
                 cajas[x, y] = caja;
                 n++;
             }
     }
 
-    void hacerCambio(int x, int y)
+    void CambiarClick(int x, int y)
     {
         int dx = getDX(x, y);
         int dy = getDY(x, y);
+        Cambiar(x, y, dx, dy);
+        Debug.Log(Victoria());
+    }
 
+    void Cambiar(int x, int y, int dx, int dy)
+    {       
         var from = cajas[x, y];
         var target = cajas[x + dx, y + dy];
 
@@ -38,6 +46,7 @@ public class Puzzle15 : MonoBehaviour
 
         from.ActualizarPos(x + dx, y + dy);
         target.ActualizarPos(x, y);
+
     }
 
     int getDX(int x, int y)
@@ -64,5 +73,66 @@ public class Puzzle15 : MonoBehaviour
             return -1;
         }
         return 0;
+    }
+
+    void Shuffle()
+    {
+        for (int i=0; i<4; i++)
+        {
+            for (int j=0; j<4; j++)
+            {
+                if (cajas[i, j].estaVacio())
+                {
+                    Vector2 pos = movValido(i, j);
+                    Cambiar(i, j, (int)pos.x,(int)pos.y);
+                }
+            }
+        }
+    }
+
+    Vector2 movValido(int x, int y)
+    {
+        Vector2 pos = new Vector2();
+        do
+        {
+            int n = Random.Range(0, 4);
+            if (n == 0)
+                pos = Vector2.left;
+            else if (n == 1)
+                pos = Vector2.right;
+            else if (n == 2)
+                pos = Vector2.up;
+            else if (n == 3)
+                pos = Vector2.down;
+        }while(!(rangoValido(x + (int)pos.x) && rangoValido(y + (int)pos.y)) || repetido(pos));
+        lastMove = pos;
+        return pos;
+    }
+
+    private Vector2 lastMove;
+
+    bool rangoValido (int n)
+    {
+        return n >= 0 && n <= 3;
+    }
+
+    bool repetido (Vector2 pos)
+    {
+        return pos * -1 == lastMove;
+    }
+
+    bool Victoria()
+    {
+        int n = 0;
+        for (int y = 3; y >= 0; y--)
+            for (int x = 0; x < 4; x++)
+            {
+                if (cajas[x, y].gameObject.name != "caja " + n)
+                {
+                    return false;
+                }
+                n++;
+            }
+        return true;
     }
 }
