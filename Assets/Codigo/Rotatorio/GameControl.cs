089ec7e27e355a05a952fd7d0bfb2f00e0fb3cdf
Daniel Navarrete <daniel_navarrete_05@hotmail.com>
2021-01-29 22:14:21 -0600

    PORFIN ESTA ESTA MADREEEEEEE (puzzle rotatorio con cambio de escena)

--- Assets/Codigo/Rotatorio/GameControl.cs
+++ Assets/Codigo/Rotatorio/GameControl.cs
@@ -9,6 +9,12 @@ public class GameControl : MonoBehaviour
     [SerializeField]
     private Transform[] pictures;
 
+    public bool Imagen0Funciona;
+    public bool Imagen1Funciona;
+    public bool Imagen2Funciona;
+    public bool Imagen3Funciona;
+    public bool Imagen4Funciona;
+    public bool Imagen5Funciona;
 
     public static bool youWin;
 
@@ -22,13 +28,73 @@ public class GameControl : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (pictures[0].transform.rotation.z == 0)
+        //imagen 0
+        if (pictures[0].transform.rotation.z == 1)
+        {
+            Imagen0Funciona = true;
+        }
+        if (pictures[0].transform.rotation.z == -1)
+        {
+            Imagen0Funciona = true;
+        }
+
+        //imagen 1
+        if(pictures[1].transform.rotation.z == 1)
+        {
+            Imagen1Funciona = true;
+        }
+        if (pictures[1].transform.rotation.z == -1)
+        {
+            Imagen1Funciona = true;
+        }
+
+        //imagen 2
+        if (pictures[2].transform.rotation.z == 1)
+        {
+            Imagen2Funciona = true;
+        }
+        if (pictures[2].transform.rotation.z == -1)
+        {
+            Imagen2Funciona = true;
+        }
+
+        //imagen 3
+        if (pictures[3].transform.rotation.z == 1)
+        {
+            Imagen3Funciona = true;
+        }
+        if (pictures[3].transform.rotation.z == -1)
+        {
+            Imagen3Funciona = true;
+        }
+
+        //imagen 4
+        if (pictures[4].transform.rotation.z == 1)
+        {
+            Imagen4Funciona = true;
+        }
+        if (pictures[4].transform.rotation.z == -1)
+        {
+            Imagen4Funciona = true;
+        }
+
+        //imagen 5
+        if (pictures[5].transform.rotation.z == 1)
+        {
+            Imagen5Funciona = true;
+        }
+        if (pictures[5].transform.rotation.z == -1)
+        {
+            Imagen5Funciona = true;
+        }
+
+        //Victoria
+
+        if (Imagen0Funciona == true && Imagen1Funciona == true && Imagen2Funciona == true && Imagen3Funciona == true && Imagen4Funciona == true && Imagen5Funciona == true)
         {
             youWin = true;
             SceneManager.LoadScene("Simon dice");
         }
- 
-
     }
 
 }
