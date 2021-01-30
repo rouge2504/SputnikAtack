16844c1b33e889fc25d67333516e03dc48d520e4
rouge2504 <rouge2504@gmail.com>
2021-01-29 23:34:39 -0600

    Enemy IA

---
+++ Assets/Codigo/IA_Enemy/BulletEnemy.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletEnemy : MonoBehaviour
+{
+    [HideInInspector]  public Vector3 targetPos;
+    [SerializeField] private float speed = 3;
+    [SerializeField] private float timeToDie = 3;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Destroy(this.gameObject, timeToDie);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MoveToTarget();
+    }
+
+    public void MoveToTarget()
+    {
+        transform.Translate((targetPos - transform.position).normalized * Time.deltaTime * speed);
+    }
+}
