8a3f32fa0a220238f23d8ffd8d74f7813d902e2c
Daniel Navarrete <daniel_navarrete_05@hotmail.com>
2021-01-31 14:51:04 -0600

    Bad y god funcional

--- Assets/Codigo/IA_Enemy/Boss3.cs
+++ Assets/Codigo/IA_Enemy/Boss3.cs
@@ -118,6 +118,7 @@ public class Boss3 : MonoBehaviour
         if (VariablesGlobales.Vida_enemigo_fase3 == 0)
         {
             VariablesGlobales.Enemigo_fase3_existe = false;
+            SceneManager.LoadScene("God1");
         }
     }
 }
