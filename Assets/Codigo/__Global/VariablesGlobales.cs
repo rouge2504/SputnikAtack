716cca190125b3f0cd385f64395e856b6cd8640a
Fernando747 <fernandojcr5@gmail.com>
2021-01-30 22:47:47 -0600

    Auto stash before merge of "main" and "origin/main"

---
+++ Assets/Codigo/__Global/VariablesGlobales.cs
@@ -0,0 +1,204 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class VariablesGlobales 
+{
+
+    public static int Cuero_azar
+    {
+        get
+        {
+            return Cuero_azar;
+        }
+
+        set
+        {
+            Cuero_azar = value;
+        }
+    }
+
+    public static int Sello_azar
+    {
+        get
+        {
+            return Sello_azar;
+        }
+
+        set
+        {
+            Sello_azar = value;
+        }
+    }
+    public static int Corona_azar
+    {
+        get
+        {
+            return Corona_azar;
+        }
+
+        set
+        {
+            Corona_azar = value;
+        }
+    }
+
+    public static bool Cuero
+    {
+        get
+        {
+            return Cuero;
+        }
+
+        set
+        {
+            Cuero = value;
+        }
+    }
+
+    public static bool Sello
+    {
+        get
+        {
+            return Sello;
+        }
+
+        set
+        {
+            Sello = value;
+        }
+    }
+
+    public static bool Corona
+    {
+        get
+        {
+            return Corona;
+        }
+
+        set
+        {
+            Corona = value;
+        }
+    }
+
+    public static bool Talisman
+    {
+        get
+        {
+            return Talisman;
+        }
+
+        set
+        {
+            Talisman = value;
+        }
+    }
+
+    public static bool Llave1
+    {
+        get
+        {
+            return Llave1;
+        }
+
+        set
+        {
+            Llave1 = value;
+        }
+    }
+
+    public static bool Llave2
+    {
+        get
+        {
+            return Llave2;
+        }
+
+        set
+        {
+            Llave2 = value;
+        }
+    }
+
+    public static bool Zona1_bloqueada
+    {
+        get
+        {
+            return Zona1_bloqueada;
+        }
+
+        set
+        {
+            Zona1_bloqueada = value;
+        }
+    }
+
+    public static bool Zona2_bloqueada
+    {
+        get
+        {
+            return Zona2_bloqueada;
+        }
+
+        set
+        {
+            Zona2_bloqueada = value;
+        }
+    }
+
+    public static float Vida_jugador
+    {
+        get
+        {
+            return Vida_jugador;
+        }
+
+        set
+        {
+            Vida_jugador = value;
+        }
+    }
+
+    public static float Vida_enemigo_fase1
+    {
+        get
+        {
+            return Vida_enemigo_fase1;
+        }
+
+        set
+        {
+            Vida_enemigo_fase1 = value;
+        }
+    }
+
+    public static float Vida_enemigo_fase2
+    {
+        get
+        {
+            return Vida_enemigo_fase2;
+        }
+
+        set
+        {
+            Vida_enemigo_fase2 = value;
+        }
+    }
+
+    public static float Vida_enemigo_fase3
+    {
+        get
+        {
+            return Vida_enemigo_fase3;
+        }
+
+        set
+        {
+            Vida_enemigo_fase3 = value;
+        }
+    }
+
+
+
+}
